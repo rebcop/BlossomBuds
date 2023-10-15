463a8e6b593e9e505b2067c523e64a78b909fb02
rebcop <76892971+rebcop@users.noreply.github.com>
2023-10-15 18:32:55 -0400

    Revamped home and app page.

---
+++ Archive/site.js
@@ -0,0 +1,163 @@
+// get values from user input
+function getValues() {
+
+    // get Blossom Value
+    let blossomValue = document.getElementById('blossomValue').value;
+
+    // get Buds Value
+    let budsValue = document.getElementById('budsValue').value;
+
+    // get Start Value
+    let startValue = document.getElementById('startValue').value;
+
+    // get Stop Value 
+    let stopValue = document.getElementById('stopValue').value;
+
+    // how many columns to display in the table
+    let colNum = 3;
+
+    // change string values to number
+    blossomValue = parseInt(blossomValue);
+    budsValue = parseInt(budsValue);
+    startValue = parseInt(startValue);
+    stopValue = parseInt(stopValue);
+
+
+    if (Number.isInteger(blossomValue) && Number.isInteger(budsValue) && Number.isInteger(startValue) && Number.isInteger(stopValue) && stopValue > startValue) {
+
+        let blossomBuds = generateBlossomBuds(blossomValue, budsValue, startValue, stopValue, colNum);
+
+        displayBlossomBuds(blossomBuds);
+
+    } else {
+        // Uh oh! Something is wrong
+        Swal.fire ({
+            icon: 'error',
+            bakcdrop: false,
+            title: 'Oops!',
+            text: 'Please enter valid integers'
+        })
+
+    }
+
+}
+
+// generate the numbers 1-100
+// if number divisible by both values display 'BlossomBuds'
+// else if number divisible by Blossom Value display 'Blossom'
+// else if number divisible by Buds Value display 'Buds'
+// else add number
+function generateBlossomBuds(blossomValue, budsValue, startValue, stopValue, colNum) {
+
+    // array to hold values
+    let blossomBuds = [];
+    let blossomBudsRow = [];
+    let countCol = 1;
+    
+    // generate the numbers 1-100 replacing values respectively
+    for (let n = startValue; n <= stopValue; n++) {
+
+            if ( n % blossomValue == 0 && n % budsValue == 0 ) {
+
+                // Add 'BlossomBuds' instead of number
+                blossomBudsRow.push('BlossomBuds');
+    
+            } else if ( n % blossomValue == 0) {
+    
+                // Add 'Blossom' instead of number
+                blossomBudsRow.push('Blossom');
+
+    
+            } else if ( n % budsValue == 0 ) {
+    
+                // Add 'Buds' instead of number
+                blossomBudsRow.push('Buds');
+
+            } else {
+                
+                // Add number
+                blossomBudsRow.push(n);
+            }
+
+            
+            if (countCol == colNum) {
+                countCol = 1;
+                blossomBuds.push(blossomBudsRow);
+                blossomBudsRow = [];
+
+            } else if (n == stopValue)  {
+                blossomBuds.push(blossomBudsRow);
+            } else {
+                countCol++;
+            }
+        
+    }
+
+    return blossomBuds;
+
+}
+
+// display the BlossomBuds on the page
+function displayBlossomBuds(blossomBuds) {
+
+    // declare variables
+    let blossomBudsHtml = '';
+    
+    let className = '';
+
+    // loop over array to add values in array to html
+    // check if value is 'BlossomBuds' change class to 'blossom-buds' & add html cell contents
+    // if value is 'Blossom' change class to 'blossom' & add html cell contents
+    // if value is 'buds' change class to 'buds' & add html cell contents
+
+    for (let row = 0; row < blossomBuds.length; row++) {
+
+        let blossomBudsHtmlRow = '';
+
+        for (let col = 0; col < blossomBuds[row].length; col++) {
+
+            if ( blossomBuds[row][col] == 'BlossomBuds') {
+
+                // specify class name for cells with 'BlossomBuds' value
+                className = 'blossom-buds';
+    
+                // specify value of cell along with additional html needed for cell
+                blossomBudsCellHtml = `<i class="bi bi-flower3"></i>${blossomBuds[row][col]}<i class="bi bi-flower3"></i>`;
+    
+            } else if ( blossomBuds[row][col] == 'Blossom' ) {
+    
+                // specify class name for cells with 'Blossom' value
+                className = 'blossom';
+    
+                // specify value of cell along with additional html needed for cell
+                blossomBudsCellHtml = blossomBuds[row][col];
+    
+            } else if ( blossomBuds[row][col] == 'Buds' ) {
+    
+                // specify class name for cells with 'Buds' value
+                className = 'buds';
+    
+                // specify value of cell along with additional html needed for cell
+                blossomBudsCellHtml = blossomBuds[row][col];
+    
+            } else {
+    
+                // specify class name for all other cells
+                className = '';
+    
+                // specify value of cell along with additional html needed for cell
+                blossomBudsCellHtml = blossomBuds[row][col];
+            }
+
+            blossomBudsHtmlRow += `<td class="${className}">${blossomBudsCellHtml}</td>`;
+
+        }
+
+        blossomBudsHtml += `<tr>${blossomBudsHtmlRow}</tr>`;
+
+    }
+
+    // get the innerHTML of the element we want to put it in and update it
+    document.getElementById('results').innerHTML = blossomBudsHtml;
+
+}
--- js/site.js
+++ js/site.js
@@ -1,162 +1,128 @@
-// get values from user input
+// get input values from the user
 function getValues() {
 
-    // get Blossom Value
-    let blossomValue = document.getElementById('blossomValue').value;
+    // get the Blossom Value input
+    let blossomValue = document.getElementById('blossomValue').value; // gets value as a string
 
-    // get Buds Value
-    let budsValue = document.getElementById('budsValue').value;
+    // get the Buds Value input
+    let budsValue = document.getElementById('budsValue').value; // gets value as a string
 
-    // get Start Value
-    let startValue = document.getElementById('startValue').value;
+    // get the Start Value input
+    let startValue = document.getElementById('startValue').value; // gets value as a string
 
-    // get Stop Value 
-    let stopValue = document.getElementById('stopValue').value;
+    // get the Stop Value input
+    let stopValue = document.getElementById('stopValue').value; // gets value as a string
 
-    let colNum = 3;
-
-    // change string values to number
+    // change all string inputs to numbers
     blossomValue = parseInt(blossomValue);
     budsValue = parseInt(budsValue);
     startValue = parseInt(startValue);
     stopValue = parseInt(stopValue);
 
-
-    if (Number.isInteger(blossomValue) && Number.isInteger(budsValue) && Number.isInteger(startValue) && Number.isInteger(stopValue) && stopValue > startValue) {
-
-        let blossomBuds = generateBlossomBuds(blossomValue, budsValue, startValue, stopValue, colNum);
-
-        displayBlossomBuds(blossomBuds);
-
+    // check if inputs recieved are what is expected
+    if ( Number.isInteger(blossomValue) && Number.isInteger(budsValue) && Number.isInteger(startValue) && Number.isInteger(stopValue) && startValue <= stopValue && startValue >= -9999 && stopValue <= 9999 ) {
+        
+        // get generated values based on inputs
+        let generatedValues = generateValues(blossomValue, budsValue, startValue, stopValue);
+        
+        displayValues(generatedValues);
     } else {
-        // Uh oh! Something is wrong
+        // Something is wrong
         Swal.fire ({
             icon: 'error',
-            bakcdrop: false,
+            backdrop: false,
             title: 'Oops!',
-            text: 'Please enter valid integers'
+            text: 'Please enter valid integers, with a Stop Value less than the Start Value. Start and Stop Values can only be between and including -9,999 and 9,999.',
+            confirmButtonColor: '#cd6a6a'
         })
-
     }
 
 }
 
-// generate the numbers 1-100
-// if number divisible by both values display 'BlossomBuds'
-// else if number divisible by Blossom Value display 'Blossom'
-// else if number divisible by Buds Value display 'Buds'
-// else add number
-function generateBlossomBuds(blossomValue, budsValue, startValue, stopValue, colNum) {
+// generate values based on the input from user
+function generateValues(blossomValue, budsValue, startValue, stopValue) {
 
-    // array to hold values
-    let blossomBuds = [];
-    let blossomBudsRow = [];
-    let countCol = 1;
-    
-    // generate the numbers 1-100 replacing values respectively
+    // declare variable to hold generated values
+    let generatedValues = [];
+
+    // generate values from stopValue to startValue
     for (let n = startValue; n <= stopValue; n++) {
-
-            if ( n % blossomValue == 0 && n % budsValue == 0 ) {
-
-                // Add 'BlossomBuds' instead of number
-                blossomBudsRow.push('BlossomBuds');
-    
-            } else if ( n % blossomValue == 0) {
-    
-                // Add 'Blossom' instead of number
-                blossomBudsRow.push('Blossom');
-
-    
-            } else if ( n % budsValue == 0 ) {
-    
-                // Add 'Buds' instead of number
-                blossomBudsRow.push('Buds');
-
-            } else {
-                
-                // Add number
-                blossomBudsRow.push(n);
-            }
-
-            
-            if (countCol == colNum) {
-                countCol = 1;
-                blossomBuds.push(blossomBudsRow);
-                blossomBudsRow = [];
-
-            } else if (n == stopValue)  {
-                blossomBuds.push(blossomBudsRow);
-            } else {
-                countCol++;
-            }
         
-    }
+        // check each value
+        // if value is a multiple of blossomValue and budsValue
+        if (n % blossomValue == 0 && n % budsValue == 0) {
 
-    return blossomBuds;
+            // add "BlossomBuds" to the array
+            generatedValues.push('BlossomBuds');
 
-}
+        // if value is a multiple of blossomValue
+        } else if ( n % blossomValue == 0) {
 
-// display the BlossomBuds on the page
-function displayBlossomBuds(blossomBuds) {
+            // aadd "Blossom" to the array
+            generatedValues.push('Blossom');
 
-    // declare variables
-    let blossomBudsHtml = '';
-    
-    let className = '';
+        // if value is a multiple of budsValue
+        } else if ( n % budsValue == 0 ) {
 
-    // loop over array to add values in array to html
-    // check if value is 'BlossomBuds' change class to 'blossom-buds' & add html cell contents
-    // if value is 'Blossom' change class to 'blossom' & add html cell contents
-    // if value is 'buds' change class to 'buds' & add html cell contents
+            // add "Buds" into array
+            generatedValues.push('Buds');
 
-    for (let row = 0; row < blossomBuds.length; row++) {
-
-        let blossomBudsHtmlRow = '';
-
-        for (let col = 0; col < blossomBuds[row].length; col++) {
-
-            if ( blossomBuds[row][col] == 'BlossomBuds') {
-
-                // specify class name for cells with 'BlossomBuds' value
-                className = 'blossom-buds';
-    
-                // specify value of cell along with additional html needed for cell
-                blossomBudsCellHtml = `<i class="bi bi-flower3"></i>${blossomBuds[row][col]}<i class="bi bi-flower3"></i>`;
-    
-            } else if ( blossomBuds[row][col] == 'Blossom' ) {
-    
-                // specify class name for cells with 'Blossom' value
-                className = 'blossom';
-    
-                // specify value of cell along with additional html needed for cell
-                blossomBudsCellHtml = blossomBuds[row][col];
-    
-            } else if ( blossomBuds[row][col] == 'Buds' ) {
-    
-                // specify class name for cells with 'Buds' value
-                className = 'buds';
-    
-                // specify value of cell along with additional html needed for cell
-                blossomBudsCellHtml = blossomBuds[row][col];
-    
-            } else {
-    
-                // specify class name for all other cells
-                className = '';
-    
-                // specify value of cell along with additional html needed for cell
-                blossomBudsCellHtml = blossomBuds[row][col];
-            }
-
-            blossomBudsHtmlRow += `<td class="${className}">${blossomBudsCellHtml}</td>`;
+        } else {
+            // value into array
+            generatedValues.push(n);
 
         }
 
-        blossomBudsHtml += `<tr>${blossomBudsHtmlRow}</tr>`;
+    } 
+
+    return generatedValues;
+
+}
+
+// display values back to user
+function displayValues(generatedValues) {
+
+    // declare variable to hold content to be displayed
+    let blossomBudsHtml = '';
+
+
+    // Add each generated value with html needed to make cols to blossomBudsHtml
+    for (let i = 0; i < generatedValues.length; i++) {
+
+        // declare variable for styling element as needed
+        let styleClass = '';
+
+        // if value is 'BlossomBuds' assign class 'blossom-buds'
+        if ( generatedValues[i] == 'BlossomBuds') {
+
+            styleClass = 'col-1 d-flex justify-content-center align-items-center blossom-buds';
+
+            generatedValues[i] = `<span>Buds</span><i class="bi bi-flower3"></i>`;
+
+        // if value is 'Blossom' assign class 'blossom'
+        } else if ( generatedValues[i] == 'Blossom' ) {
+
+            styleClass = 'col-1 d-flex justify-content-center align-items-center blossom';
+
+            generatedValues[i] = `<i class="bi bi-flower3"></i>`;
+
+            // if value is 'Buds' assign class 'buds'
+        } else if ( generatedValues[i] == 'Buds' ) {
+
+            styleClass = 'col-1 d-flex justify-content-center align-items-center buds';
+
+        // for everything else assign class 'number'
+        } else {
+
+            styleClass = 'col-1 d-flex justify-content-center align-items-center number';
+
+        }
+
+        blossomBudsHtml += `<div class="col mx-1 ${styleClass}">${generatedValues[i]}</div>`;
 
     }
 
-    // get the innerHTML of the element we want to put it in and update it
+    // reassign contents of HTML to generatedValues with HTML accordingly
     document.getElementById('results').innerHTML = blossomBudsHtml;
 
 }
